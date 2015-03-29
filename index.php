1c8b774b5b82abe4cbfd43901a00fdd840d9c845
hagmar1512 <redgum2@gmail.com>
2015-03-29 20:47:11 +0100

    change crlf

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <body>
         <p>Hello World</p>
         <?php
-            echo "HTTP_HOST=".$_SERVER['HTTP_HOST']."/n";
-            echo "SERVER_ADDR=".$_SERVER['SERVER_ADDR']."/n";
+            echo "HTTP_HOST=".$_SERVER['HTTP_HOST']."\n";
+            echo "SERVER_ADDR=".$_SERVER['SERVER_ADDR']."\n";
 		  ?>
     </body>
 </html>
