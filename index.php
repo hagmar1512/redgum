a74af57209f1c7ed20864b434a54731dbb1b79cd
hagmar1512 <redgum2@gmail.com>
2015-03-29 20:44:16 +0100

    Added server_addr

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <body>
         <p>Hello World</p>
         <?php
-            echo $_SERVER['HTTP_HOST']."\n";
-            echo $_SERVER['SERVER_NAME'];
+            echo "HTTP_HOST=".$_SERVER['HTTP_HOST']."/n";
+            echo "SERVER_ADDR=".$_SERVER['SERVER_ADDR']."/n";
 		  ?>
     </body>
 </html>
