8ab366850da8427ef0f0288d821f30f0e91a8f56
hagmar1512 <redgum2@gmail.com>
2015-03-29 18:57:52 +0100

    Test file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>redgum test</title>
+    </head>
+    <body>
+        <p>Hello World</p>
+        <?php
+            echo $_SERVER['HTTP_HOST'];
+            echo $_SERVER['SERVER_ADDR'];
+		  ?>
+    </body>
+</html>
